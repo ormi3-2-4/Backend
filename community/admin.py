b08e104db68494df4885e9b9d70e9f1e901d93e2
gangto33 <142385671+gangto33@users.noreply.github.com>
2023-12-12 19:57:26 +0900

    ➕Add: admin 추가
    - 모델 전부 추가.

--- community/admin.py
+++ community/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Community
+from .models import Community, Comment, Like
 
 admin.site.register(Community)
+admin.site.register(Comment)
+admin.site.register(Like)
