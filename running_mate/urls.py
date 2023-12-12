11c50369484fe9f440e9d426d837ee2715bdab40
hoyonzz <129498722+hoyonzz@users.noreply.github.com>
2023-12-13 01:42:17 +0900

    🚚 urls.py 내용 수정

--- running_mate/urls.py
+++ running_mate/urls.py
@@ -14,7 +14,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('user/', include('user.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
