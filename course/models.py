bf0dafd514f17fb8a8d16ec58e47c8f746b4fdc9
UkiDelly <daehyeon1999@naver.com>
2023-12-12 23:30:52 +0900

    🚚 update: Record 모델
    - created_at 컬럼 추가
    - kind을 class 형태의 choice으로 변경
    - Record, Record Images db이름 지정
    - migration 폴더 내 모든 파일 무시 처리

--- course/models.py
+++ course/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-from Record.models import Record
+from record.models import Record
 
 User = get_user_model()
 
@@ -13,7 +13,9 @@ class Course(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, help_text="생성 날짜 및 시간")
     view_count = models.PositiveIntegerField(default=0, help_text="조회수")
     likes = models.ManyToManyField(User, related_name="liked_courses", help_text="좋아요")
-    tags = models.ManyToManyField("Tag", related_name="courses", help_text="태그", blank=True)
+    tags = models.ManyToManyField(
+        "Tag", related_name="courses", help_text="태그", blank=True
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="작성자")
     record = models.ForeignKey(Record, on_delete=models.CASCADE, help_text="운동 기록")
 
