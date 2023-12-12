0b8a946196958d50c29e5e4e30bbc1d507601e84
JiMan Park <jmp7911@gmail.com>
2023-12-12 17:42:49 +0900

    :heavy_plus_sign: Add: [Record] urls.py serializers.py

--- record/views.py
+++ record/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import RecordSerializer
+from .models import Record
 
-# Create your views here.
+class RecordViewSet(ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
