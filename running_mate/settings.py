d5ec4f207dbce39384d3dd82f6d9093f7a543a0b
hoyong.shin <hoyong0511@gmail.com>
2023-12-13 01:03:40 +0900

    create_user/serializers.py, update_settings.py

--- running_mate/settings.py
+++ running_mate/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-}
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -145,3 +141,9 @@ REST_FRAMEWORK = {
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+}
+
+AUTH_USER_MODEL = 'users.User'
