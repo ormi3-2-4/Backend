2fea5094f827c07686bc86c5745b4197fbe271cc
gangto33 <142385671+gangto33@users.noreply.github.com>
2023-12-13 02:05:41 +0900

    ➕ Add: admin 추가

--- recommend/admin.py
+++ recommend/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import Recommend, RecommendComment, RecommendLike, Category
+
+admin.site.register(Recommend)
+admin.site.register(RecommendComment)
+admin.site.register(RecommendLike)
+admin.site.register(Category)
