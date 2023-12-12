0cae9dd3ec2a39c2fd0f9b5235b27dc22a0471e5
JiMan Park <jmp7911@gmail.com>
2023-12-12 16:13:29 +0900

    :adhesive_bandage: Fix: 네이밍컨벤션에 맞게 수정

--- record/models.py
+++ record/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-class record(models.Model):
+class Record(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
