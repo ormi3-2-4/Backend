cd05508293fa349c6a7d6207c995430bd4c861c1
JiMan Park <jmp7911@gmail.com>
2023-12-12 14:51:39 +0900

    :heavy_plus_sign: Add: pillow 모듈 설치 , model 오타 수정

--- Record/models.py
+++ Record/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import get_user_model
+from django.contrib.auth import get_user_model
 
 class Record(models.Model):
   user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
