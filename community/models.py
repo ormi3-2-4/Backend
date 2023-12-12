fdbe214445a34162a9b79210b5bfd92d9e69c6d6
gangto33 <142385671+gangto33@users.noreply.github.com>
2023-12-12 19:23:18 +0900

    ➕Add: Comment 모델 추가
    - Record 모델 import

--- community/models.py
+++ community/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from record.models import Record
 
 User = get_user_model()
 
@@ -24,3 +25,26 @@ class Community(models.Model):
     class Meta:
         verbose_name = '게시글'
         verbose_name_plural = '커뮤니티'
+
+
+class Comment(models.Model):
+    commu_id = models.ForeignKey(
+        Community, on_delete=models.CASCADE, related_name='comments'
+        )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    content = models.TextField()
+    parent_comment = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.content}: {self.user}: {self.created_at}'
+
+
+    class Meta:
+        verbose_name = '댓글'
+        verbose_name_plural = '커뮤니티 댓글'
