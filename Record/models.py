0e1f8b173e7f29d4749b953c6fa0a4a4fa6e3cb7
JiMan Park <jmp7911@gmail.com>
2023-12-12 14:25:39 +0900

    :heavy_plus_sign: Modify: Record user 필드 수정, settings.py Record 추가

--- Record/models.py
+++ Record/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import get_user_model
 
 class Record(models.Model):
-  user_id = models.CharField(max_length=50)
+  user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
   created_at = models.DateTimeField(auto_now_add=True)
   last_update = models.DateTimeField(auto_now=True)
   finished_at = models.DateTimeField(blank=True, null=True)
