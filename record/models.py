3a3598f9ed13fee5196a626da8c3e48454ed57dc
JiMan Park <jmp7911@gmail.com>
2023-12-12 17:56:05 +0900

    :truck: Rename: Record field명 rename, RecordImage 추가

--- record/models.py
+++ record/models.py
@@ -3,17 +3,26 @@ from django.contrib.auth import get_user_model
 
 class Record(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_update = models.DateTimeField(auto_now=True)
-    finished_at = models.DateTimeField(blank=True, null=True)
+    start_at = models.DateTimeField(auto_now_add=True)
+    end_at = models.DateTimeField(blank=True, null=True)
     static_map = models.ImageField(upload_to='record/static_map/%Y/%m/%d/')
-    data = models.TextField(help_text='GPS데이터')
+    coords = models.TextField(help_text='GPS데이터')
     distance = models.FloatField(help_text='운동한 거리')
     speed = models.FloatField(help_text='평균 속력')
-    kind = models.CharField(help_text='운동 종류', max_length=50)
+    kind = models.CharField(help_text='enum(walk, run, bicycle)', max_length=50) 
 
     def __str__(self):
-        return f'{self.user}: {self.created_at}'
+        return f'{self.user}: {self.start_at}'
 
     class Meta:
         verbose_name = '운동 기록'
+
+class RecordImage(models.Model):
+    record = models.ForeignKey(Record, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='record/images/%Y/%m/%d/')
+
+    def __str__(self):
+        return f'{self.record} - {self.image}'
+
+    class Meta:
+        verbose_name = '인증샷'
