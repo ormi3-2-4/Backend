b32367bb0225b898bed56b0a10e0492219ee246e
hoyonzz <129498722+hoyonzz@users.noreply.github.com>
2023-12-13 01:52:47 +0900

    🚚 models.py 오타수정

--- user/models.py
+++ user/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     nickname = models.CharField(max_length=20)
-    image = models.ImageField(blank=True, upload_to='user/images')
+    profile_image = models.ImageField(blank=True, upload_to='user/images')
