870132aeb4420432ade1bcafe3bcc096428a54da
JiMan Park <jmp7911@gmail.com>
2023-12-13 17:31:31 +0900

    :adhesive_bandage: Fix: 사용하지 않는 username field를 UserSerializer에서 삭제

--- user/serializers.py
+++ user/serializers.py
@@ -8,7 +8,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'nickname', 'profile_image')
+        fields = ('id', 'email', 'password', 'nickname', 'profile_image')
         extra_kwargs = {'password': {'write_only': True}}
 
         def create(self, validated_data):
