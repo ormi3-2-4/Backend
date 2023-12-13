eef791418bc23372855a560ccc0238c9dbba61b3
UkiDelly <daehyeon1999@naver.com>
2023-12-13 13:11:51 +0900

    🚚 update: Recomment 모델 업데이트

--- community/models.py
+++ community/models.py
@@ -8,12 +8,14 @@ User = get_user_model()
 
 class Community(models.Model):
     record = models.ForeignKey(to=Record, on_delete=models.CASCADE, help_text="운동기록")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="community")
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    view_count = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="community", help_text="작성자"
+    )
+    title = models.CharField(max_length=100, help_text="제목")
+    content = models.TextField(help_text="내용")
+    created_at = models.DateTimeField(auto_now_add=True, help_text="생성날짜 및 시간")
+    updated_at = models.DateTimeField(auto_now=True, help_text="업데이트 날짜 및 시간")
+    view_count = models.PositiveIntegerField(default=0, help_text="조회수")
     likes = models.ManyToManyField(
         User, related_name="community_likes", help_text="좋아요"
     )
@@ -29,13 +31,18 @@ class Community(models.Model):
 
 class CommunityComment(models.Model):
     community = models.ForeignKey(
-        Community, on_delete=models.CASCADE, related_name="comments"
+        Community,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        help_text="운동게시글 댓글",
     )
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    parent_comment = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="작성자")
+    content = models.TextField(help_text="내용")
+    parent_comment = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, help_text="부모 댓글"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, help_text="생성날짜 및 시간")
+    updated_at = models.DateTimeField(auto_now=True, help_text="업데이트 날짜 및 시간")
 
     def __str__(self):
         return f"{self.content}: {self.user}: {self.created_at}"
--- recommend/models.py
+++ recommend/models.py
@@ -1,76 +1,59 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
 
 class Recommend(models.Model):
+    class Category(models.TextChoices):
+        SHOES = "SHOES", "운동화"
+        CLOTHES = "CLOTHES", "운동복"
+        BICYCLE = "BICYCLE", "자전거"
+
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='recommend'
-        )
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    view_count = models.PositiveIntegerField(default=0)
+        User, on_delete=models.CASCADE, related_name="recommend", help_text="작성자"
+    )
+    title = models.CharField(max_length=100, help_text="제목")
+    content = models.TextField(help_text="내용")
+    created_at = models.DateTimeField(auto_now_add=True, help_text="생성날짜 및 시간")
+    updated_at = models.DateTimeField(auto_now=True, help_text="업데이트 날짜 및 시간")
+    view_count = models.PositiveIntegerField(default=0, help_text="조회수")
+    likes = models.ManyToManyField(
+        User, related_name="recommend_likes", help_text="좋아요"
+    )
     image = models.ImageField(
-        upload_to='recommend/images/%Y/%m/%d/', blank=True
-        )
-    category = models.ForeignKey(
-        'Category', null=True, blank=True, on_delete=models.SET_NULL
-        )
+        upload_to="recommend/images/%Y/%m/%d/", blank=True, help_text="이미지"
+    )
+    category = models.CharField(
+        choices=Category.choices, max_length=10, help_text="용품 종류"
+    )
 
     def __str__(self):
-        return f'{self.title}: {self.user}: {self.created_at}'
-
+        return f"{self.title}: {self.user}: {self.created_at}"
 
     class Meta:
-        verbose_name = '게시글'
-        verbose_name_plural = 'Recommend 게시판'
+        verbose_name = "게시글"
+        verbose_name_plural = "Recommend 게시판"
 
 
 class RecommendComment(models.Model):
-    recommend_id = models.ForeignKey(
-        Recommend, on_delete=models.CASCADE, related_name='comments'
-        )
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE
+    recommend = models.ForeignKey(
+        Recommend,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        help_text="추천게시글 댓글",
     )
-    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="작성자")
+    content = models.TextField(help_text="내용")
     parent_comment = models.ForeignKey(
-        'self', on_delete=models.CASCADE, null=True
-        )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return f'{self.content}: {self.user}: {self.created_at}'
-
-
-    class Meta:
-        verbose_name = '댓글'
-        verbose_name_plural = 'Recommend 댓글'
-
-
-class RecommendLike(models.Model):
-    recommend_id = models.ForeignKey(
-        Recommend, on_delete=models.CASCADE, related_name='comments'
-        )
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
-    like = models.PositiveIntegerField(default=0)
-
-
-    class Meta:
-        verbose_name = 'Like'
-        verbose_name_plural = 'Recommend Likes'
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=25, unique=True)
+        "self", on_delete=models.CASCADE, null=True, help_text="부모 댓글"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, help_text="생성날짜 및 시간")
+    updated_at = models.DateTimeField(auto_now=True, help_text="업데이트 날짜 및 시간")
 
     def __str__(self):
-        return self.name
-    
+        return f"{self.content}: {self.user}: {self.created_at}"
+
     class Meta:
-        verbose_name = '카테고리'
-        verbose_name_plural = 'Recommend 카테고리'
+        verbose_name = "댓글"
+        verbose_name_plural = "Recommend 댓글"
