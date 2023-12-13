f3f13d6b87febcabfd59c5bd7928887e95a5b7e3
UkiDelly <daehyeon1999@naver.com>
2023-12-13 11:40:24 +0900

    ➕ add: consumer.py 추가
    - http왁 ws 분리

--- running_mate/asgi.py
+++ running_mate/asgi.py
@@ -10,13 +10,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from channels.routing import ProtocolTypeRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+from running_mate.consumer import RunningMateWebSocket
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "running_mate.settings")
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "ws": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(RunningMateWebSocket.as_asgi()),
     }
 )
