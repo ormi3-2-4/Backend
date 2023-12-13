6507ac7d29387d9e7ee4fd4c01a8a054a9840543
UkiDelly <daehyeon1999@naver.com>
2023-12-13 18:28:55 +0900

    ✨ add: course.urls 등록

--- user/views.py
+++ user/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework import permissions
+
 from .serializers import UserSerializer
 
 User = get_user_model()
+
 
 class UserCreateView(generics.CreateAPIView):
     queryset = User.objects.all()
