434c6c1fc03de79e6541c01e52dd4ae4b002ffda
gangto33 <142385671+gangto33@users.noreply.github.com>
2023-12-20 16:45:12 +0900

    🩹 Fix: 커뮤니티 모델 수정
    - record 에 null=True 추가

--- community/models.py
+++ community/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 
 class Community(models.Model):
-    record = models.ForeignKey(to=Record, on_delete=models.CASCADE, help_text="운동기록")
+    record = models.ForeignKey(to=Record, on_delete=models.CASCADE, null=True, help_text="운동기록")
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="community", help_text="작성자"
     )
@@ -51,3 +51,4 @@ class CommunityComment(models.Model):
         db_table = "community_comments"
         verbose_name = "댓글"
         verbose_name_plural = "Community 댓글"
+
