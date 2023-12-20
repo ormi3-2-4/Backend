ee3581dcfee4b9c6a5dc236718743614117a94ac
UkiDelly <daehyeon1999@naver.com>
2023-12-20 10:22:01 +0900

    🩹 fix: 로그인 API의 응답이 user 객체가 아닌 문제 해결

--- user/views.py
+++ user/views.py
@@ -31,7 +31,7 @@ class UserLoginView(APIView):
             token = TokenObtainPairSerializer.get_token(user)
             access_token = str(token.access_token)
             data = {
-                "user": serializer.data,
+                "user": UserSerializer(user).data,
                 "token": {
                     "access_token": access_token,
                     "refresh_token": str(token),
