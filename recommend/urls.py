4b17d9c21eea294e4dc5a303de0a3e41982c5dfe
gangto33 <142385671+gangto33@users.noreply.github.com>
2023-12-22 10:51:57 +0900

    ➕ Add: urls.py 추가

---
+++ recommend/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from recommend.views import RecommendCommentView, RecommendView
+
+recommend_router = DefaultRouter()
+recommend_router.register("", RecommendView, basename="recommend")
+
+recommend_comment_router = DefaultRouter()
+recommend_comment_router.register(
+    "",
+    RecommendCommentView,
+    basename="recommend-comment",
+)
+
+
+urlpatterns = [
+    path("", include(recommend_router.urls)),
+    path("comment/", include(recommend_comment_router.urls)),
+]
